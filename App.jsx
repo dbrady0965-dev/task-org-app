5ad0d55d34bf67a24eb94446b6ed3b18c57e9520
dbrady0965-dev <d.brady0965@gmail.com>
2026-02-09 15:41:49 -0700

    add authgate for logn

--- App.jsx
+++ App.jsx
@@ -1,7 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-
+import AuthGate from "./AuthGate";
+<Route 
+  path="/dashboard"
+  element={
+    <AuthGate>
+      <Dashboard />
+    </AuthGate>
+  }
+/>
 export default function App() {
   return (
     <Routes>
